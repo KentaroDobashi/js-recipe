07a2f8c899dda4536cc3f5dcec2b7c54bed260cb
KentaroDobashi <popkentaro3232@gmail.com>
2022-04-16 18:14:58 +0900

    [□と◯の]を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -3,3 +3,8 @@ const figure = document.getElementById("figure")
 figure.onclick = function () {
   figure.classList.toggle("rounded")
 }
+
+// キーボードが押されたときに図形が変化
+document.onkeydown = function () {
+  figure.classList.toggle("rounded")
+}
